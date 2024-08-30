ab9729ad52d317e194fea05107ff8c0e60bd6b20
fatimazafarrizvi <117430408+fatimazafarrizvi@users.noreply.github.com>
2024-08-30 18:01:54 +0530

    Update user_routes.js

--- Routes/user_routes.js
+++ Routes/user_routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const user = require("../APIs/userApi")
 
-router.get("/user_all", user.userfind) //login
+router.post("/user_all", user.userfind) //login
 router.post("/user_insert", user.insert_user) //sign_up
 router.put("/user_put", user.update_user)
 router.delete("/user_delete", user.delete_user)
